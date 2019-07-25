824b1236f33b6b922703f63efa5e8876f96cd105
chrioniep <eponde26@gmail.com>
2019-07-25 15:01:20 +0100

    create projet model

--- MywebSite/website/models.py
+++ MywebSite/website/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Projet(models.Model):
+    image = models.ImageField()
+    name_url = models.URLField()
+    category = models.CharField(max_length=100)
+    
+
+    def __str__(self):
+        return self.name_url
